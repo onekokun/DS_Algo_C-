2f3ce3fa2ae8d10a48bda913b44a90f464308f26
Sameer <52498120+onekokun@users.noreply.github.com>
2020-10-03 19:06:26 +0530

    Create StaticArray.cpp

---
+++ StaticArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {1,2,3,4,5};
+
+    //accessing
+    cout<<arr[4];
+
+    //assigning
+    arr[3] = 4;
+    return 0;
+}
